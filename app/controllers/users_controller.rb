63e24790c1939e3d9dc411ae7a40e6b3d41d89b9
Steve Galbraith <steve@stevegalbraith.com>
2015-07-13 14:11:50 -0700

    squashed some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
     def create
       @user = User.new(user_params)
-      @user.company = @user.location.company
+      # @user.company = @user.location.company
       if @user.save
         session[:user_id] = @user.id
         redirect_to root_path
